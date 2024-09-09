16c29a8daee2716b68ff0bc814b636efbcd2ea41
mateus_castelanp@hotmail.com <adasdada@asdasda.com>
2024-09-09 15:22:48 -0300

    first commit

---
+++ src/api/routes/articles.js
@@ -0,0 +1,141 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const articlesSchema = new mongoose.Schema({
+  article_title: { type: String },
+  article_body: { type: String },
+  article_keywords: { type: String },
+  article_liked_count: { type: Number, default: 0 },
+  article_featured: { type: Boolean },
+  article_summary: { type: String },
+  article_author_email: { type: String },
+  article_author_name: { type: String },
+  article_author_id: { type: String },
+  article_published_date: { type: Date, default: Date.now }
+});
+
+articlesSchema.index({ article_keywords: 'text' })
+
+const Article = mongoose.model('Article', articlesSchema);
+
+router.post('/cadastro', async (req, res) => {
+  const article = req.body;
+  try {
+    const newArticle = await Article.create(article);
+    console.log('Objeto salvo com sucesso!');
+    res.json({ message: 'Artigo salvo com sucesso!', newArticle });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+
+router.get('/search', async (req, res) => {
+  try {
+    const keywords = req.query.keywords;
+
+    const foundArticles = await Article.find({
+      $text: { $search: keywords },
+    });
+
+    res.status(200).json(foundArticles);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.get('/all', async (req, res) => {
+  try {
+    const foundArticles = await Article.find();
+    console.log('Objetos encontrados com sucesso!');
+    res.status(200).json(foundArticles);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+
+
+router.get('/',  async (req, res) => {
+  try {
+    let foundArticles;
+
+    foundArticles = await Article.find();
+
+    // Se o usuário for um administrador, obtenha todos os artigos
+    if (req.session.user && req.session.user.author_level === 'admin') {
+      foundArticles = await Article.find();
+    } else if (req.session.user) {
+      // Se não for um administrador, obtenha apenas os artigos do usuário atual
+      foundArticles = await Article.find({ article_author_id: req.session.user._id });
+    } else {
+      // Caso o usuário não esteja autenticado, retorne um erro ou uma mensagem apropriada
+      return res.status(401).json({ message: 'Usuário não autenticado.' });
+    }
+
+    console.log('Objetos encontrados com sucesso!');
+    res.status(200).json(foundArticles);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.post('/like/:pid', async (req, res) => {
+  const pid = req.params.pid;
+
+  try {
+    const updatedArticle = await Article.findByIdAndUpdate(
+      pid,
+      { $inc: { article_liked_count: 1 } },
+      { new: true }
+    );
+    res.json({ message: 'Like adicionado com sucesso!', updatedArticle });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.get('/:pid', async (req, res) => {
+  const pid = req.params.pid;
+  try {
+    const foundArticle = await Article.findById(pid);
+    console.log('Objeto encontrado com sucesso!');
+    res.json({ message: 'Artigo encontrado com sucesso!', foundArticle });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.put('/:pid', async (req, res) => {
+  const pid = req.params.pid;
+  const newArticle = req.body;
+  console.log(newArticle);
+  try {
+    const updatedArticle = await Article.findByIdAndUpdate(pid,
+      {
+        article_title: newArticle.article_title,
+        article_body: newArticle.article_body,
+        article_keywords: newArticle.article_keywords,
+        article_published: newArticle.article_published,
+        article_featured: newArticle.article_featured,
+        article_author_email: newArticle.article_author_email,
+      }, { new: true });
+    console.log('Objeto Atualizado:', updatedArticle);
+    res.json({ message: 'Artigo alterado com sucesso!', updatedArticle });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.delete('/delete/:articleId', async (req, res) => {
+  const articleId = req.params.articleId;
+  try {
+    const deletedArticle = await Article.findByIdAndDelete(articleId);
+    res.json({ message: 'Artigo excluído com sucesso!', deletedArticle });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+module.exports = router;
---
+++ src/api/routes/users.js
@@ -0,0 +1,162 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+
+router.use(cors(corsOptions));
+
+const usersSchema = new mongoose.Schema({
+  author_name: String,
+  author_email: String,
+  author_user: String,
+  author_pwd: String,
+  author_level: String,
+  author_status: Boolean,
+  author_create_date: { type: Date, default: Date.now }
+});
+
+const User = mongoose.model('User', usersSchema);
+
+
+router.post('/cadastro', async (req, res) => {
+  const user = req.body;
+  
+  console.log("Tentativa de cadastro", user)
+
+  try {
+    if (user.author_pwd) {
+      const hashedPassword = await bcrypt.hash(user.author_pwd, 10);
+      user.author_pwd = hashedPassword;
+    }
+ 
+    const newUser = await User.create(user);
+    console.log('Objeto salvo com sucesso!');
+    res.json({ message: 'Usuário salvo com sucesso!', newUser });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  console.log('Tentativa de login:', username);
+
+  try {
+    const user = await User.findOne({ author_user: username });
+
+    if (user && user.author_status) {
+      const verifyPassword = await bcrypt.compare(password, user.author_pwd);
+      if(verifyPassword){
+        req.session.user = {
+          _id: user._id,
+          author_name: user.author_name,
+          author_email: user.author_email,
+          author_level: user.author_level
+        };
+        res.status(200).json({ message: 'Login bem sucecido!'})
+      } 
+    } else {
+      res.status(401).json({ message: 'Credenciais inválidas' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.get('/session', (req, res) => {
+  try {
+    if (req.session && req.session.user) {
+      res.status(200).json({ user: req.session.user });
+    } else {
+      res.status(200).json({ user: null });
+    }
+  } catch (error) {
+    console.error('Erro ao obter detalhes da sessão:', error.message);
+    res.status(500).json({ error: 'Erro ao obter detalhes da sessão' });
+  }
+});
+
+
+router.post('/logout', (req, res) => {
+  req.session.destroy ((err) => {
+    if (err) {
+      console.error('Erro ao encerrar a sessão:', err);
+      res.status(500).json({ error: 'Erro ao encerrar a sessão' });
+    } else {
+      res.status(200).json({ message: 'Logout bem-sucedido!' });
+    }
+  });
+});
+
+router.get('/getAll', async (req, res) => {
+  try {
+    const users = await User.find();
+    console.log('Objetos encontrados com sucesso!');
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.get('/:pid', async (req, res) => {
+  const pid = req.params.pid;
+  try {
+    const foundUser = await User.findById( pid );
+    console.log('Objeto encontrado com sucesso!');
+    res.json({ message: 'Usuário encontrado com sucesso!', foundUser });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.put('/:pid', async (req, res) => {
+  const pid = req.params.pid;
+  const newUser = req.body;
+  console.log(newUser);
+
+  if (newUser.author_pwd) {
+    const hashedPassword = await bcrypt.hash(newUser.author_pwd, 10);
+    newUser.author_pwd = hashedPassword;
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(pid, 
+      { 
+        author_name: newUser.author_name, 
+        author_email: newUser.author_email,
+        author_pwd: newUser.author_pwd,
+        author_level: newUser.author_level,
+        author_status: newUser.author_status,
+      }, { new: true });
+    console.log('Objeto Atualizado:', updatedUser);
+    res.json({ message: 'Usuário alterado com sucesso!', updatedUser });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.delete('/:pid', async (req, res) => {
+  const pid = req.params.pid;
+  try {
+    const deletedUser = await User.findByIdAndDelete(pid);
+    console.log('Objeto deletado:', deletedUser);
+    res.json({ message: 'Usuário deletado com sucesso!', deletedUser });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+
+
+
+module.exports = router;
